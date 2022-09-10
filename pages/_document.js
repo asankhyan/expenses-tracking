64137ad463194dd1de7296740ada26b89a88457a
Ankush Kumar <ankush.kumar@cloudsmartz.net>
2022-09-10 21:40:04 +0530

    page title changed

--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,9 @@ import { Html, Head, Main, NextScript } from 'next/document';
 export default function DefaultPage(){
     return(
         <Html>
-            <Head/>
+            <Head>
+               <title>Expenses Tracking</title> 
+            </Head>
             <body>
                 <Main/>
                 <NextScript/>
